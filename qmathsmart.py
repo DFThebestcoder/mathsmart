e69fb41d70297824aa090e5d3fea2b1cb82ea967
DFThebestcoder <darsh.fuletra@gmail.com>
2026-06-30 11:17:27 -0700

    v7

--- qmathsmart.py
+++ qmathsmart.py
@@ -1,11 +1,11 @@
 import statistics
 while True:
     user_text = input(
-        "choose to calculate the Surface Area, volume, mmmar, or addition: "
+        "choose to calculate the Surface Area, volume, mmmar, addition, or substraction: "
     ).strip().lower()
-    if user_text in ("surface area", "volume", "mmmar", "addition"):
+    if user_text in ("surface area", "volume", "mmmar", "addition", "substraction"):
         break
-    print("Please type only: surface area, volume, mmmar, or addition.")
+    print("Please type only: surface area, volume, mmmar, addition, or substraction.")
 if user_text == "surface area":
     while True:
         user_text = input(
@@ -190,3 +190,16 @@ elif user_text == "addition":
             print("Please enter valid numbers, like 3.5 or 4.")
 
     print("The sum of the numbers is:", sum(numbers))
+elif user_text == "substraction":
+    print("----------------------------------------------------")
+    print("this program calculates the substraction of numbers.")
+    print("----------------------------------------------------")
+    while True:
+        user_text = input("put your numbers in the box separated by -: ")
+        try:
+            numbers = [float(x) for x in user_text.split('-')]
+            break
+        except ValueError:
+            print("Please enter valid numbers, like 3.5 or 4.")
+            
+    print("your answer is:", numbers[0] - sum(numbers[1:]))
